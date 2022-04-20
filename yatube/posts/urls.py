fda82e985536235a6f788dc7c091b262c8c6027c
Леонид Винокуров <leoneed@me.com>
2022-04-20 21:34:50 +0500

    Добавил namespace

--- yatube/posts/urls.py
+++ yatube/posts/urls.py
@@ -2,11 +2,14 @@ from django.urls import path
 
 from . import views
 
+#yatube/posts/urls.py
+app_name = 'posts'
+
 urlpatterns = [
-    path('', views.index),
-    path('index.html', views.index),
-    path('group_list.html/index.html', views.index),
-    path('group_list.html/group_list.html', views.group_list),
-    path('group_list.html/', views.group_list),
-    path('group/<slug:slug>/', views.group_posts),
+    path('', views.index, name=''),
+    path('index.html', views.index, name='index'),
+    path('group_list.html/index.html', views.index, name='group_list_index'),
+    path('group_list.html/group_list.html', views.group_list, name='group_group'),
+    path('group_list.html/', views.group_list, name='group_list'),
+    path('group/<slug:slug>/', views.group_posts, name='group_posts'),
 ]
--- yatube/yatube/urls.py
+++ yatube/yatube/urls.py
@@ -16,7 +16,8 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import include, path
 
+#yatube/urls.py
 urlpatterns = [
-    path('', include('posts.urls')),
+    path('', include('posts.urls', namespace='posts')),
     path('admin/', admin.site.urls),
 ]
