8038e74e56d16d8765f83c1ded572360a56f618f
Леонид Винокуров <leoneed@me.com>
2022-04-20 20:18:52 +0500

    Использовал словари context

--- yatube/posts/views.py
+++ yatube/posts/views.py
@@ -4,12 +4,25 @@ from django.shortcuts import render
 
 def index(request):
      template = 'posts/index.html'
-     return render(request, template)
+     title = 'Это главная страница проекта Yatube'
+     text = 'Последние обновления на сайте'
+     context = {
+          'title': title,
+          'text': text,
+     }
+     return render(request, template, context)
 
 
 def group_list(request):
      template = 'posts/group_list.html'
-     return render(request, template)
+     group_title = 'Группы проекта'
+     group_text = 'Здесь будет информация о группах проекта Yatube'
+     context = {
+          'title': group_title,
+          'text': group_text,
+     }
+
+     return render(request, template, context)
 
 
 def group_posts(request, blogger):
